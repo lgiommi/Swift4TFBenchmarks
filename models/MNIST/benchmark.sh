31baf3f0420006039ff599fb53f61c0e081ac562
lgiommi <luca.giommi2@studio.unibo.it>
2020-12-14 17:13:27 +0100

    Changed results.json location

--- models/MNIST/benchmark.sh
+++ models/MNIST/benchmark.sh
@@ -13,7 +13,7 @@ printf "Argument params is %s\n" "$2"
 
 cd ./SwiftML
 rootPath=`pwd`
-swift run swift-ml LeNet -p ${rootPath}/../params.json
+swift run swift-ml LeNet -p ${rootPath}/../../../params.json
 cd -
 python3 ./Keras/LeNet.py --params $2
 python3 ./Pytorch/LeNet_v2.py --params $2
